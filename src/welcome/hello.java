48693fa0396b161368d377da19dbf600be68f423
kamatchimargy <122144070+kamatchimargy@users.noreply.github.com>
2023-01-07 16:09:21 -0800

    second commit

--- src/welcome/hello.java
+++ src/welcome/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Margy");
 		System.out.println("By Kamini");
+		System.out.println("Hiiiiiiii.....");
 
 	}
 
